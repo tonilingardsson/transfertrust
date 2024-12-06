00ee79d22fd686ba53e3a0003e6378181e05ef23
0xtoni <antonio.lingardsson@minima.global>
2024-12-06 16:02:50 +0100

    Installed Hardhat in the right place

--- transfertrust/src/App.jsx
+++ transfertrust/src/App.jsx
@@ -8,30 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>TransferTrust</h1>
-        <h3>
-          A decentralized application (dApp) for secure verification and
-          transfer of real assets
-        </h3>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      <h1>TransferTrust</h1>
+      <h3>
+        A decentralized application (dApp) for secure verification and transfer
+        of real assets
+      </h3>
+      <button>Start</button>
+      <p>
+        This application is a part of my final project for my remote studies as
+        a Blockchain Developer at Medie Institutet, Stockholm
       </p>
     </>
   );
