971cf871d489e72b78c2ee66dfebccdecf1be430
Antonio Lingårdsson <toni.lingardsson@gmail.com>
2024-12-10 19:28:31 +0100

    Created balances page and updated app.jsx and index.js; work in progress

--- transfertrust/src/index.js
+++ transfertrust/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 // Import Moralis
 import Moralis from "moralis";
+import cors from "cors";
 // Import the EvmChain dataType
 import { EvmChain } from "@moralisweb3/common-evm-utils";
 // Import dotenv
@@ -10,6 +11,12 @@ dotenv.config({ path: './src/.env' });
 const app = express();
 const port = 3000;
 
+// Allow access to React app domain
+app.use(cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+}));
+
 // Add a variable for the API key, address, and chain
 const MORALIS_API_KEY = process.env.moralisAPI;
 const address = process.env.address;
@@ -17,6 +24,7 @@ const chain = EvmChain.SEPOLIA;
 
 console.log("Moralis API Key:", MORALIS_API_KEY);
 console.log("Address:", address);
+
 
 async function getDemoData() {
     // Get native balance
@@ -60,6 +68,33 @@ app.get("/demo", async (req, res) => {
         const data = await getDemoData();
         res.status(200);
         res.json(data);
+    } catch (error) {
+        // Handle errors
+        console.error(error);
+        res.status(500);
+        res.json({ error: error.message });
+    }
+});
+
+app.get("/balances", async (req, res) => {
+    try {
+        // Promise.all() for receiving data async from two endpoints
+        const [nativeBalance, tokenBalances] = await Promise.all([
+            Moralis.EvmApi.balance.getNativeBalance({
+                chain: EvmChain.ETHEREUM,
+                address,
+            }),
+            Moralis.EvmApi.token.getWalletTokenBalances({
+                chain: EvmChain.ETHEREUM,
+                address,
+            }),
+        ]);
+        res.status(200).json({
+            // formatting the output
+            address,
+            nativeBalance: nativeBalance.result.balance.ether,
+            tokenBalances: tokenBalances.result.map((token) => token.display()),
+        });
     } catch (error) {
         // Handle errors
         console.error(error);
