6451131e4016b69c18fb37ece4f20a688ad7fd80
Antonio Lingårdsson <toni.lingardsson@gmail.com>
2025-02-03 08:56:00 +0100

    Updated TransferTrust.sol adding ERC721 from oppenzeppelin

--- contracts/TransferTrust.sol
+++ contracts/TransferTrust.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract TransferTrust is ReentrancyGuard, ERC721Enumerable, Ownable {
+contract TransferTrust is ReentrancyGuard, ERC721Enumerable is ERC721, Ownable {
     // Struct to store comprehensive car details
     struct CarDetails {
         string vin;
